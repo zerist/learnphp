4e7c899a182892cbf372f135032401e3550a231d
zerist <15871792739@163.com>
2019-10-26 23:00:21 +0800

    1

--- test.php
+++ test.php
@@ -1,12 +1,3 @@
 <?php
-    $contact = array(1, "xk", "lebby", "beijing", "010", "11");
-    echo '<table border="1" width="600" align="center">';
-    echo '<caption><h1>联系人列表</h1></caption>';
-    echo '<tr bgcolor="#dddddd">';
-    echo '<th>编号</th><th>姓名</th><th>公司</th><th>地址</th><th>电话</th><th>email</th>';
-    echo '</tr><tr>';
-    for($i=0; $i<count($contact); $i++){
-        echo '<td>'.$contact[$i].'</td>';
-    }
-    echo '</tr></table>';
+    print_r($_SERVER);
 ?>
