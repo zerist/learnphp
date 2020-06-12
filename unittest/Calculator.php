ac6d561baa524249f7a9f6a4d74d4a67a1b58d76
zerist <15871792739@163.com>
2020-06-12 20:06:44 +0800

    test

---
+++ unittest/Calculator.php
@@ -0,0 +1,20 @@
+<?php
+class Calculator{
+    /**
+     * @assert(0, 0) == 0
+     * @assert(1, 2) == 4
+     */
+    /**
+     * @param $a
+     * @param $b
+     * @return mixed
+     */
+    public function add($a, $b){
+        return $a + $b;
+    }
+
+    public function add2($a, $b){
+        return $a + $b;
+    }
+
+}
